cf507853936227c1a8e991b0097dcb1c5cc8ab64
imp <yujiarui2009@qq.com>
2020-06-02 21:45:41 +0800

    one

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <!--  -->
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>姓名{{apis.name}}</p>
+    <p>qq:{{apis.qq}}</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      apis:{}
+    }
+  },
+  created(){
+    axios.get(`/api/datas`)
+      .then(res=>{
+        console.log(res)
+        this.apis = res.data
+      })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? './' : './',
+    outputDir: 'dist',
+    assetsDir: 'static',
+    indexPath: 'index.html',
+    filenameHashing: true,
+    // 反向代理
+    devServer: {
+      open: true,
+      host: 'localhost',
+      port: 8080,
+      https: false,
+      hotOnly: false,
+      proxy: {
+        // 配置跨域
+        '/api': {
+          target: 'http://39.99.202.101/api/',
+          ws: true,
+          changOrigin: true,
+          pathRewrite: {
+            '^/api': ''
+          }
+        }
+      },
+      before: app => {}
+    }
+  };
+  
